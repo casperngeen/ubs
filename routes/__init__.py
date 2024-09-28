ede7e7baf38a3ee6195c6ea6d406ffb284533f53
Casper Ng <casperngeen@gmail.com>
2024-09-29 00:33:24 +0800

    update init

--- routes/__init__.py
+++ routes/__init__.py
@@ -11,9 +11,5 @@ import routes.digital_colony
 import routes.dodge_bullet
 import routes.kazuma
 import routes.mailtime
-<<<<<<< HEAD
 import routes.decode
-
-=======
 import routes.lisp_parser
->>>>>>> 6ebe6a71e96a3088c0299437f2881475286b9ea9
