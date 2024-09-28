bfc7ce061a84f2a9aaffb792712d586f5d98ad7a
NgZiXin <ngzixin02@gmail.com>
2024-09-28 15:11:25 +0800

    Change

--- routes/dodge_bullet.py
+++ routes/dodge_bullet.py
@@ -1,18 +1,17 @@
 import logging
+from flask import Flask, request, jsonify
 
-from flask import request, jsonify
+app = Flask(__name__)
 
-from routes import app
-
-logger = logging.getLogger(__name__)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+app.logger.setLevel(logging.INFO)
 
 @app.route('/dodge', methods=['POST'])
 def evaluate_dodge():
     data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    print(data)
-
+    app.logger.info("Data received for evaluation: %s", data)
     return jsonify([])
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
