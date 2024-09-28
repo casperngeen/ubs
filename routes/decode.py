7192c36d4b03e59d722b9d51c68fe37dca209210
NgZiXin <ngzixin02@gmail.com>
2024-09-29 00:31:11 +0800

    Change

--- routes/decode.py
+++ routes/decode.py
@@ -12,7 +12,7 @@ def decode():
             "flag": "UB5{w3lc0m3_70_c7f_N0ttyB01}"
         },
         "openAiExploration": {
-            "flag": "sk-ilOvESpoRTs"
+            "flag": "vn-ilOvESpoRTs"
         },
         "dictionaryAttack": {
             "flag": "UB5{FLAG_CONTENT_HERE}",
