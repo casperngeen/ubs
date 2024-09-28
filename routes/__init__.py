2064ce9923e11b79e3e055330c7f77ff14898b3e
yizhong187 <yizhong187@gmail.com>
2024-09-28 15:54:35 +0800

    commit

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,9 @@
-import routes.thetourist
-import routes.klotski
-import routes.wordle
-import routes.square
 from flask import Flask
 
 app = Flask(__name__)
+
+import routes.square
+import routes.wordle
+import routes.klotski
+import routes.thetourist
+import routes.mailtime
