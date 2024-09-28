ab62ae465efa6dc59b5fa55d4cd1efb2a764d8a2
NgZiXin <ngzixin02@gmail.com>
2024-09-28 15:17:46 +0800

    Change

--- routes/dodge_bullet.py
+++ routes/dodge_bullet.py
@@ -1,4 +1,5 @@
 import logging
+
 from flask import request, jsonify
 
 from routes import app
@@ -9,7 +10,36 @@ logger = logging.getLogger(__name__)
 def evaluate_dodge():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    return jsonify([])
+    
+    results = []
+    for test_case in data:
+        generations = test_case["generations"]
+        colony = test_case["colony"]
+
+        # Solve for only generation 10 :(
+        generations = 10
+
+        while generations > 0:
+            next_colony_arr = ['_' for _ in range(len(colony) * 2 - 1)]
+            curr_colony_index_counter = 0
+            new_colony_index_counter = 1
+            print(colony)
+            for i in range(len(colony) - 1):
+                weight = sum(int(digit) for digit in colony)
+                next_colony_arr[new_colony_index_counter] = str((weight + (int(colony[i]) - int(colony[i + 1]))) % 10)
+                next_colony_arr[curr_colony_index_counter] = colony[i]
+                next_colony_arr[curr_colony_index_counter + 2] = colony[i + 1]
+                new_colony_index_counter += 2
+                curr_colony_index_counter += 2
+            colony = ''.join(next_colony_arr)
+            generations -= 1
+        results.append(sum(int(digit) for digit in colony))
+
+    return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)   
+    app.run(debug=True)    
+
+
+                
+
